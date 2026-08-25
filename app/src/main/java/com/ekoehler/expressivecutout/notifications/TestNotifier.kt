c4f6a6834799511627725e070ca1dd674e3c3994
Anurag Deshpande <adeshpande@idfbins.com>
2026-08-24 21:54:08 -0600

    [FEAT] Display checkmark and download complete status in expanded notification view

--- app/src/main/java/com/ekoehler/expressivecutout/notifications/TestNotifier.kt
+++ app/src/main/java/com/ekoehler/expressivecutout/notifications/TestNotifier.kt
@@ -244,10 +244,22 @@ object TestNotifier {
         progressJob = scope.launch {
             var current = 0
             while (true) {
+                val isDone = current >= PROGRESS_MAX
+                val displayTitle = if (isDone) {
+                    appContext.getString(R.string.test_progress_complete_title)
+                } else {
+                    title
+                }
+                val displayText = if (isDone) {
+                    appContext.getString(R.string.test_progress_complete_text)
+                } else {
+                    text
+                }
+
                 val notification = NotificationCompat.Builder(appContext, CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_stat_island)
-                    .setContentTitle(title)
-                    .setContentText(text)
+                    .setContentTitle(displayTitle)
+                    .setContentText(displayText)
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setOnlyAlertOnce(true)
                     .setProgress(PROGRESS_MAX, current, false)
@@ -261,15 +273,15 @@ object TestNotifier {
                 IslandEventBus.emit(
                     CutoutSignal.Notification(
                         packageName = appContext.packageName,
-                        title = title,
-                        text = text,
+                        title = displayTitle,
+                        text = displayText,
                         key = PROGRESS_KEY,
                         smallIcon = Icon.createWithResource(appContext, R.drawable.ic_stat_island),
                         progressData = ProgressData(
                             max = PROGRESS_MAX,
                             current = current,
                             isIndeterminate = false,
-                            title = title,
+                            title = displayTitle,
                         ),
                     ),
                 )
